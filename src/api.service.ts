bf7f99398bd8849656542c815826ed32b2e4ccd1
Per Christian Bjelke <perchristian.bjelke@skatteetaten.no>
2025-06-29 20:20:14 +0200

    main: added env

--- src/api.service.ts
+++ src/api.service.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://api.kasselars.com/';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+const MESSAGE_SERVICE_PATH = '/message';
 
 export async function fetchMessage(): Promise<string> {
-    const response = await axios.get(API_BASE_URL);
-    return response.data.message;
+    try {
+        const response = await axios.get(`${API_BASE_URL}${MESSAGE_SERVICE_PATH}`);
+        return response.data.message;
+    } catch (error) {
+        console.error('Error fetching message:', error);
+        throw error;
+    }
 }
